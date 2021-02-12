2c5ae1d4786f9dc5ee8f094cc43239dc10d31728
darrylt <4dtait@gmail.com>
2021-02-12 11:09:52 -0700

    sets up initial routes

--- main_app/views.py
+++ main_app/views.py
@@ -5,5 +5,31 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+class Bird:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+
+birds = [
+    Bird("Nimbus", "gray", "sweet", 5),
+    Bird("Ghost", "orange", "fat", 12),
+    Bird("Bon Bon", "maine coon", "the boss", 0),
+    Bird("Pancho", "yellow-nape", "a talker!", 7),
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html")
+    # return HttpResponse("<h1>Hello World</h1>")
+
+
+def about(request):
+    # return HttpResponse("<h1>About the Bird Collector</h1>")
+    return render(request, "about.html")
+
+
+def index(request):
+    return render(request, "birds/index.html", {"birds": birds})
