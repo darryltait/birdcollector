a411e618d6834180f8b4a493b51cc15ba3ec3b66
darrylt <4dtait@gmail.com>
2021-02-17 11:11:05 -0700

    adds crud to birds

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -13,6 +14,9 @@ class Bird(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"bird_id": self.id})
 
 
 class Feeding(models.Model):
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bird
 
 # from django.http import HttpResponse
@@ -24,3 +25,20 @@ def birds_index(request):
 def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
     return render(request, "birds/detail.html", {"bird": bird})
+
+
+class BirdCreate(CreateView):
+    model = Bird
+    fields = "__all__"
+    #  can redirect like below, but it's better to add a return in Model
+    # success_url = "/birds/"
+
+
+class BirdUpdate(UpdateView):
+    model = Bird
+    fields = ["breed", "description", "age"]
+
+
+class BirdDelete(DeleteView):
+    model = Bird
+    success_url = "/birds/"
