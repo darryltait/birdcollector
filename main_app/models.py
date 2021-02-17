f87671eef79a8e54f636345b69ace9dee4756630
darrylt <4dtait@gmail.com>
2021-02-17 09:25:59 -0700

    adds admin page and feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+MEALS = (("B", "Breakfast"), ("L", "Lunch"), ("D", "Dinner"))
+
 
 class Bird(models.Model):
     name = models.CharField(max_length=250)
@@ -11,3 +13,12 @@ class Bird(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Feeding(models.Model):
+    date = models.DateField("feeding date")
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("birds/", views.birds_index, name="birds_index"),
-    path("birds/<int:bird_id>/", views.birds_detail, name="birds_detail"),
+    path("birds/", views.birds_index, name="index"),
+    # path("birds/", views.birds_index, name="birds_index"),
+    path("birds/<int:bird_id>/", views.birds_detail, name="detail"),
+    # path("birds/<int:bird_id>/", views.birds_detail, name="birds_detail"),
 ]
