1c6b54920c0720d7abc3f24c97b2b59488ebb1da
darrylt <4dtait@gmail.com>
2021-02-18 14:35:59 -0700

    adds feeding feature to a bird

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 
@@ -18,6 +19,9 @@ class Bird(models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={"bird_id": self.id})
 
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
 
 class Feeding(models.Model):
     date = models.DateField("feeding date")
@@ -26,3 +30,6 @@ class Feeding(models.Model):
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ["-date"]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bird
+from .forms import FeedingForm
 
 # from django.http import HttpResponse
 
@@ -24,7 +25,10 @@ def birds_index(request):
 
 def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
-    return render(request, "birds/detail.html", {"bird": bird})
+    feeding_form = FeedingForm()
+    return render(
+        request, "birds/detail.html", {"bird": bird, "feeding_form": feeding_form}
+    )
 
 
 class BirdCreate(CreateView):
@@ -42,3 +46,12 @@ class BirdUpdate(UpdateView):
 class BirdDelete(DeleteView):
     model = Bird
     success_url = "/birds/"
+
+
+def add_feeding(request, bird_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.bird_id = bird_id
+        new_feeding.save()
+    return redirect("detail", bird_id=bird_id)
